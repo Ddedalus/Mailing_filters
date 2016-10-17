6550fbccaba02305e47c1888921d09ef11e03460
Ladrone Vincet <mateusz77488@gmail.com>
2016-10-17 21:54:51 +0200

    Adding Bootstrap, base for adding css for index
    Added boostrap files, base for adding css to the index, gear untouched, DOM corrected to be css compatibile.

--- index.php
+++ index.php
@@ -1,30 +1,49 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
+<!DOCTYPE HTML>                                                                 
+<html>                                                                
+<head>                                                                          
   <meta charset="utf-8"/>
-  <title>#DoWszystkich</title>
-</head>
-<body>
-  <h2>Generator Filtrów</h2>
-  <form action="gear.php" method="post">
-    Klasa:<br/>
-    <input name="class_name"/>
-    <br/>
-     
-    Adres skrzynki pośredniczącej:<br/>
-    <input name="class_email"/>
-    <br/><hr/><br/>
-     
-    Adresy uczniow<br/>
-    <textarea cols="40" rows="5" name="students"></textarea>
-    <br/><hr/><br/>
-     
-    Adresy nauczycieli<br/>
-    <textarea cols="40" rows="5" name="teachers"></textarea>
-    <br/><hr/><br/>
-     
-    <input type="submit" value="Send"/>
-  </form>
-  
-</body>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <title>#DoWszystkich</title>0
+</head>                                                                         
+<body>                                                                          
+  <div class="container">                                                       
+    <div class="row">                                                    
+      <div class="col-md-12">                                                          
+        <h2>Generator Filtrów</h2>                                              
+      </div>                                                                    
+      <div class="col-md-12">                                                    
+        <p>Lorem ipsum  monus</p>
+      </div>                                                                    
+    </div>                                                                      
+    <div class="row">                                                     
+      <form action="gear.php" method="post">                                    
+         <div class="col-md-12">
+           <div>
+	     <h3>ssKlasa:</h3>
+	   </div>
+	   <div><input name="class_name"/></div>
+         </div>                                                            
+         <div class="col-md-12">
+	   <div>
+	     <h3>Adres skrzynki pośredniczącej:</h3>
+	   </div>
+	   <div><input name="class_email"/></div>
+	 </div>                                                            
+         <div class="col-md-6">
+	   <div>
+	     <h3>Adresy uczniow</h3>
+	   </div>
+	   <div><textarea cols="40" rows="5" name="students"></textarea></div>
+	 </div>                                                            
+         <div class="col-md-6">
+	   <div>
+	     <h3>Adresy nauczycieli</h3>
+	   </div>                                                   
+	   <div><textarea cols="40" rows="5" name="teachers"></textarea></div>
+	 </div>                                                            
+         <div><input type="submit" value="Send"/></div>
+      </form>                                                                   
+    </div>                                                                      
+  </div>                                                                        
+</body>                                                                         
 </html>
